b76a2442517be6fbce92d33f110bc3a6c184403c
EmilioCarreiraOWI <221350@virtualwindow.co.za>
2024-05-09 12:48:44 +0200

    navbar not finished

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Text } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from './screens/HomeScreen';
+import DevelopmentScreen from './screens/DevelopmentScreen';
+import PhotoScreen from './screens/PhotosScreen';
+import PrivateScreen from './screens/PrivateScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    // Add navigation here
-      <HomeScreen />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='Development' component={DevelopmentScreen} />
+        <Stack.Screen name='Photos' component={PhotoScreen} />
+        <Stack.Screen name='Private' component={PrivateScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
